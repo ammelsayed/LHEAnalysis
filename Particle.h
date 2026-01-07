6cfa7e2e8eee52ef6af204359577a7c2ce735fc8
A.M.M. Elsayed <ahmedphysica@outlook.com>
2026-01-07 18:57:34 +0800

    Update Particle.h

--- Particle.h
+++ Particle.h
@@ -7,28 +7,39 @@
 class Particle : public TObject {
 public:
 
-    Int_t   pdgId, pdgId_Mother1, pdgId_Mother2;
-    Int_t   status;
+    Int_t   Status;
+    Int_t   pdgID;
+    Int_t   pdgID_Mother1;
+    Int_t   pdgID_Mother2;
+    Int_t   pdgID_Daughter1;   
+    Int_t   pdgID_Daughter2;   
 
     Float_t Px, Py, Pz, Energy, Mass;
     Float_t PT, Eta, Phi;
 
-    Int_t   Charge;
-    Int_t   Mother1, Mother2;
-    Int_t   Color1, Color2;
+    Float_t Charge;
+    TLorentzVector p4;
 
     Float_t Lifetime;
     Int_t   Helicity;
+    
+    Int_t   Color1, Color2;
 
-    TLorentzVector p4;
+    Int_t   Index;
+    Int_t   Mother1;
+    Int_t   Mother2;
+    Int_t   Daughter1;         
+    Int_t   Daughter2;         
+
 
     Particle() :
-        pdgId(0), pdgId_Mother1(0), pdgId_Mother2(0), status(0),
+        pdgID(0), pdgID_Mother1(0), pdgID_Mother2(0), Status(0),
         Px(0), Py(0), Pz(0), Energy(0), Mass(0),
         PT(0), Eta(0), Phi(0),
         Charge(0), Mother1(0), Mother2(0),
         Color1(0), Color2(0),
-        Lifetime(0), Helicity(0)
+        Lifetime(0), Helicity(0), Index(0),
+        Daughter1(0), Daughter2(0), pdgID_Daughter1(0), pdgID_Daughter2(0)
     {}
 
     const TLorentzVector& P4() const { return p4; }
